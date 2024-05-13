6f8d9c71f884dce08d9fbfec2432af5848928c00
Azyl <superj1e2z6@gmail.com>
2024-05-14 00:11:20 +0800

    base rigid enemy scene lock rotation

--- src/base_rigid_enemy.gd
+++ src/base_rigid_enemy.gd
@@ -14,9 +14,9 @@ func _ready() -> void:
 	parrybox.request_knockback.connect(apply_knockback)
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	state.angular_velocity = 0
 	state.linear_velocity = Vector2(clampf(state.linear_velocity.x,-max_speed,max_speed),state.linear_velocity.y)
 
 func apply_knockback(knockback_direction: Vector2) -> void:
 	print(knockback_direction)
-	apply_central_impulse(knockback_speed*knockback_direction.normalized())
+	var impulse:= knockback_speed*knockback_direction.normalized()
+	apply_central_impulse(impulse)
