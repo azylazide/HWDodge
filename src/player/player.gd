46666e0f1b7779c326143ea109a635cdab6456aa
Azyl <superj1e2z6@gmail.com>
2024-07-10 00:50:00 +0800

    fixed kickbox collision not being disabled in some cases after animation

--- src/player/player.gd
+++ src/player/player.gd
@@ -198,6 +198,7 @@ func _physics_process(delta: float) -> void:
 	resolve_animations()
 
 	debug_info.call_deferred()
+	print($KickBox/CollisionShape2D.disabled)
 
 func _unhandled_input(event: InputEvent) -> void:
 	movement_sm.machine_input(event)
